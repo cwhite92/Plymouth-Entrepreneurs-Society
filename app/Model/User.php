4568d2751637859df3c9584587faaffedc739ed9
Jake Champion <jakechampion@Jakes-MacBook-Air.local>
2013-02-04 08:39:10 +0000

    Added password label and confirm password field in Registration
    Added a label for password to tell users it has to be more than 6
    characters in length as well as adding a confirm password field with
    the correct validation.
    
    addded a unique email validation also and a notempty validation on the
    password field.
    
    also made the password fields empty themselves when the users clicks
    the register button for security purposes.

--- app/Model/User.php
+++ app/Model/User.php
@@ -1,7 +1,7 @@
 <?php
 
 class User extends AppModel {
-    public $validate = array(
+    var $validate = array(
         'firstname' => array(
             'required' => array(
                 'rule' => array('notEmpty'),
@@ -15,27 +15,56 @@ class User extends AppModel {
             )
         ),
         'email' => array(
-            'required' => array(
-                'rule' => array('notEmpty'),
-                'message' => 'Email is required'
+            'kosher' => array(
+                'rule' => 'email',
+                'message' => 'Please make sure your email is entered correctly.'
             ),
-            'validEmail' => array(
-                'rule' => array('email'),
-                'message' => 'Please enter a valid email'
+            'unique' => array(
+                'rule' => 'isUnique',
+                'message' => 'An account with that email already exists.'
+            ),
+            'required' => array(
+                'rule' => 'notEmpty',
+                'message' => 'Please Enter your email.'
             )
         ),
         'password' => array(
+            'required' => array(
+                'rule' => 'notEmpty',
+                'message'=>'Please enter a password.'
+            ),
             'length' => array(
                 'rule' => array('minLength', 6),
                 'message' => 'Password must be at least 6 characters in length'
             )
+        ),
+        'confirmPassword' => array(
+            'length' => array(
+                'rule' => 'validatePassword',
+                'message' => 'Passwords don\'t match'
+            )
         )
     );
+
+    function validatePassword($data) {
+        if ($this->data['User']['password'] !== $data['confirmPassword'])
+        {
+            return false;
+        }
+        return true;
+    }
 
     public function beforeSave($options = array()) {
         if (isset($this->data[$this->alias]['password'])) {
             $this->data[$this->alias]['password'] = AuthComponent::password($this->data[$this->alias]['password']);
+            unset($this->data['User']['passwd']);
         }
+
+        if (isset($this->data['User']['passwd_confirm']))
+        {
+            unset($this->data['User']['passwd_confirm']);
+        }
+
         return true;
     }
 }
--- app/View/Users/register.ctp
+++ app/View/Users/register.ctp
@@ -4,5 +4,6 @@
 <?php echo $this->form->input('firstname'); ?>
 <?php echo $this->form->input('lastname'); ?>
 <?php echo $this->form->input('email'); ?>
-<?php echo $this->form->input('password'); ?>
+<?php echo $this->form->input('password', array('between' => 'Must be atleast 6 characters', 'value' => '', 'autocomplete' => 'off')); ?>
+<?php echo $this->form->input('confirmPassword', array('type' => 'password', 'value' => '', 'autocomplete' => 'off')); ?>
 <?php echo $this->form->end('Register'); ?>
