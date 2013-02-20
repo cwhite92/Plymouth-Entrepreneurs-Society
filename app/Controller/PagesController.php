24db7c13025d4174910dffd1f72357c47344697f
Chris White <whytee92@gmail.com>
2013-02-20 02:07:00 +0000

    Latest users on homepage
    I got bored.

--- app/Controller/PagesController.php
+++ app/Controller/PagesController.php
@@ -5,7 +5,14 @@ class PagesController extends AppController {
     public $helpers = array('Html', 'Form');
 
     public function home() {
-        // This is the homepage
+        $this->loadModel('User');
+
+        // Get the latest members
+        $latestUsers = $this->User->find('all', array(
+            'order' => array('User.created'),
+            'limit' => 5
+        ));
+        $this->set('latestUsers', $latestUsers);
     }
 
 }
--- app/View/Pages/home.ctp
+++ app/View/Pages/home.ctp
@@ -3,3 +3,8 @@
 <?php if($authed): ?>
     <p>You're logged in!</p>
 <?php endif; ?>
+
+<h1>Latest users</h1>
+<?php foreach($latestUsers as $user): ?>
+    <?php echo $this->Html->link($user['Profile']['firstname'] . ' ' . $user['Profile']['lastname'], array('controller' => 'profiles', 'action' => 'view', $user['Profile']['id'])); ?><br />
+<?php endforeach; ?>
