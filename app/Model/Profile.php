9cf3bfeaf3d2dd06f9ab584d6aa9c39c55f5b25c
Jake Champion <jakechampion@Jakes-MacBook-Air.local>
2013-03-28 20:42:46 +0000

    change file ext to png

--- app/Model/Profile.php
+++ app/Model/Profile.php
@@ -56,7 +56,7 @@ class Profile extends AppModel {
     public function beforeSave($options = Array()) {
         if(isset($this->data['Profile']['picture'])) {
             // Make a filename
-            $filename = md5(microtime() * rand()) . '.jpg';
+            $filename = md5(microtime() * rand()) . '.png';
 
             // Attempt to move the uploaded file
             if(!move_uploaded_file($this->data['Profile']['picture']['tmp_name'], WWW_ROOT . 'img' . DS . 'profile_pics' . DS . $filename)) {
